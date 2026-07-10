31b969f8afa117860fc72dece5ceceed1ecdf36f
SankhiSingh <singhssankhi@gmail.com>
2026-07-10 19:51:20 +0530

    Add files via upload

---
+++ 35.cpp
@@ -0,0 +1,23 @@
+/*
+find target in array if not found then insert at sorted position
+*/
+class Solution{
+    public:
+     int searchInsert(vector<int>& nums, int target){
+        int left=0;
+        int right=mums.size()-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+             return mid;
+        }
+            else if(nums[mid]==target){
+              left=mid+1;
+        }
+         else{
+            right=mid-1;
+         }
+     }
+     return left;
+    }
+};
