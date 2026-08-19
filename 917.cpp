6ea9d224d29a3b75842651b41056f8a0ee3960fa
SankhiSingh <singhssankhi@gmail.com>
2026-08-19 08:06:16 +0530

    Add files via upload

---
+++ 917.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left=0;
+        int right=s.size()-1;
+        while(left<right){
+            if(!isalpha(s[right])){
+                right--;
+
+            }
+            else if(!isalpha(s[left])){
+                left++;
+
+            }
+            else{
+            swap(s[left],s[right]);
+            left++;
+            right--;
+            }
+        }
+        return s;
+        }
+};
