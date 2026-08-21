540f25c562032d05e048113a92915a04c78e86a4
SankhiSingh <singhssankhi@gmail.com>
2026-08-21 16:07:29 +0000

    Add files via upload

---
+++ 65.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+
+        bool seenDigit = false;
+        bool seenDot = false;
+        bool seenExp = false;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            char c = s[i];
+
+            if(isdigit(c)) {
+                seenDigit = true;
+            }
+
+            else if(c == '.') {
+
+                if(seenDot || seenExp) {
+                    return false;
+                }
+
+                seenDot = true;
+            }
+
+            else if(c == 'e' || c == 'E') {
+
+                if(!seenDigit || seenExp) {
+                    return false;
+                }
+
+                seenExp = true;
+                seenDigit = false;
+            }
+
+            else if(c == '+' || c == '-') {
+
+                if(i != 0 && s[i-1] != 'e' && s[i-1] != 'E') {
+                    return false;
+                }
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        return seenDigit;
+    }
+};
